6f09071ebd387a126e03b10fedf1c08acddd8492
matjanko <mateusz.jankowski@prokonto.pl>
2020-03-03 19:05:51 +0100

    Created project with tests

---
+++ LinqExercises.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace LinqExercises.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
